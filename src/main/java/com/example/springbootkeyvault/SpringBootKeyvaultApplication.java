3491ec328810baabb4a15a9ae95f94ff10f4090d
Julien Dubois <julien.dubois@gmail.com>
2021-05-26 14:25:52 +0200

    first commit

---
+++ src/main/java/com/example/springbootkeyvault/SpringBootKeyvaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkeyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeyvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeyvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkeyvault/SpringBootKeyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkeyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
